0571824b2af955797a111e33f48f20691b3e8c5a
danielemanuel <daniel.costea@vkernel.ro>
2017-07-03 17:37:56 +0100

    User cannot withdraw more than his initial balance

--- src/BankAccount.js
+++ src/BankAccount.js
@@ -7,13 +7,18 @@
 
     BankAccount.prototype =  {
     deposit: function(amount) {
+
       this.balance =+ amount;
       this.statement.addTransaction(amount, this.balance);
   },
 
     withdraw: function(amount) {
+      if(amount <= this.balance ){
       this.balance -= amount;
       this.statement.addTransaction(-amount, this.balance);
+    } else {
+      throw Error("Inssuficient credit!");
+    }
     },
 
     viewStatement: function(){
