67af47586e674224e14b8ee28be81ec36b861484
danielemanuel <daniel.costea@vkernel.ro>
2017-07-03 18:14:18 +0100

    Bank account TDD : * bankAccount has a balance property * bankAccount dislays the current balance * bank Account has a statement * all tests green

--- spec/BankAccountSpec.js
+++ spec/BankAccountSpec.js
@@ -3,12 +3,21 @@
 describe('BankAccount', function() {
 
     var bankAccount = new BankAccount;
+    var bankStatement = new BankStatement;
 
     it('should be an defined as an object', function() {
       expect(bankAccount instanceof(BankAccount)).toBe(true);
     });
 
+    it('has a balance property', function() {
+        expect(bankAccount.balance).toBeDefined();
+    });
 
+    it('displays the current balance property as a float', function(){
+        expect(bankAccount.balance).toBe(0.00);
+    });
 
-
-})
+    it('should have a statement', function() {
+        expect(bankAccount.statement).toBeDefined();
+    });
+});
