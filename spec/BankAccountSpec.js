28c478521f97fb2f0d0c2116cacbb7b456c8756f
danielemanuel <daniel.costea@vkernel.ro>
2017-07-03 18:05:48 +0100

    TDD : Bank account is defined as an object * Test green

---
+++ spec/BankAccountSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('BankAccount', function() {
+
+    var bankAccount = new BankAccount;
+
+    it('should be an defined as an object', function() {
+      expect(bankAccount instanceof(BankAccount)).toBe(true);
+    });
+
+
+
+
+})
