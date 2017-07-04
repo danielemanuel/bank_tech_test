020751cc8fd5fb4136b6f0a13d11c6fff1ea07fc
danielemanuel <daniel.costea@vkernel.ro>
2017-07-04 09:06:18 +0100

    * bank statement displayed properly

--- src/BankStatement.js
+++ src/BankStatement.js
@@ -1,6 +1,6 @@
 (function(exports) {
 
-  var HEADERS = 'date       || credit || debit || balance';
+  var HEADERS = 'date        || credit || debit  || balance';
 
   function BankStatement(history = new TransactionHistory()) {
     this.history = history;
@@ -15,17 +15,18 @@
       var display = HEADERS + '\n';
       this.history.transactions.forEach(function(element){
         display += formatData(element.date);
-        display += ' || ';
-        if(element.amount >0) {
+        display += '    || ';
+        if(element.amount > 0) {
+          display += element.amount.toFixed(2);
           display += ' || ';
-          display += '     ';
+          display += '      ';
           display += ' || ';
         }
         if(element.amount < 0) {
-          display += '     ';
-          display += ' || ';
+          display += '      ';
+          display += ' ||  ';
           display += Math.abs(element.amount).toFixed(2);
-          display +=' || ';
+          display +=' ||  ';
         }
         display += element.current_balance.toFixed(2);
         display += '\n';
